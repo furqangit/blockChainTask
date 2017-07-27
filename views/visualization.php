ed98832accc91c33f68b8583ebceb6b924991c67
earlvik <earlviktor@gmail.com>
2017-07-27 22:54:14 +0200

    Identify chaincode 2

--- views/visualization.php
+++ views/visualization.php
@@ -74,14 +74,22 @@
 					)
 				)
 		};
+		var chaincodes = {
+			"42602ab0d80c9fec2ed8c796989121bffc98ce51634e260d7b82d6047371da6ec9632c755da605e565f20844c13a20fcc807cd0394e5dd65063b395d38538a4c": "Carwash",
+			"8a273890a316cb8d0e42d60e05b1cd4c6d267d5b415c0adf9d031815596e3b48cf7c5efab1ea7c8662dfa03badca2881762a71ddddd94447b3e3e632c512cd77": "Park",
+			"28ce4c27f62002f5432c1b4888d6d3948e136167b4f40404e8bff24af63b766af3186d6dd7cd12f74cfa7d1d80b36bb785c7499bacf6b416348b48f40ffead81": "Uber",
+			"4a628b196cc9ec33002cc2169cba4c2bd2801b96c6f8a1c7711fa70f0cabfe4023bdcf8832d7f35b8470b6faf04e8e440123340666cef3093ef47cd73828a187": "Toll collect",
+			"aada36766f1d10343c86daac7af7fddc3c75f6710ac28c09584a53da4dd2a608d13ad07fb02aad514b891c920eb284bcc07d077b7d191aef8dad2f58bf408300": "Car"
+		}
 		var getHtmlString =  function (block,num) {
 		    var html = "<thead><tr><th>Job</th><th>tID</th><th>tTime</th><th>Service</th></tr></thead>";
 			html= html+"<tbody>";
             for (var i = 0; i < block.transactions.length; i++) {
 				var tx = block.transactions[i];
 				var id = tx.txid; // Transaction ID
+				var name = chaincodes[atob(tx.chaincodeID).substring(3)];
 				var txTime = (new Date(1000 * tx.timestamp.seconds)+"").split(" (")[0]; // Transaction time, get rid of unnecessary string
-				var payload = atob(tx.payload).substring(143).replace(/[\x00-\x1F\x7F-\x9F]/g, " ").replace(/  /g, " "); // Transaction data like "carwash pay 50"
+				var payload = name + " " + atob(tx.payload).substring(143).replace(/[\x00-\x1F\x7F-\x9F]/g, " ").replace(/  /g, " "); // Transaction data like "carwash pay 50"
 				html = html+"<tr>";
 				html = html+"<th class='col-md-1' scope='row'>"+(i+1)+"</th>";
 				html = html+"<td class='col-md-5' style='word-break:break-all;'>"+id+"</td>";
